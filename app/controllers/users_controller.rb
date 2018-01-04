b763553b65473586444e89f8039642a75e9a1295
julien-lav <julien.laville@hotmail.fr>
2018-01-04 17:47:49 +0100

    adding users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,4 +13,20 @@ class UsersController < ApplicationController
 
   def update
   end
+
+  def create
+
+    @user = User.new(user_params)
+
+    @user.current_year = params[:user][:current_year].to_i
+    @user.role = params[:user][:role] === "true" ? true : false
+
+    @user.save
+    redirect_to users_new_path
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :current_year, :role, :password, :password_confirmation)
+    end
 end
