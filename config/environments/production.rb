c7298fdd78b994eb7bcf47c2e9979940b7453404
julien-lav <julien.laville@hotmail.fr>
2018-01-09 10:45:35 +0100

    adding info/accueil page

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -73,6 +73,9 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
+  #Adding of the action mailer configuration
+  config.action_mailer.smtp_settings = { address: "info.prepa.cinema.gmail.com" }
+  config.action_mailer.default_url_options = { :host => "https://www.prepa-cinema.herokuapp.com" }
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
