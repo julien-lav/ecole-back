1a52f3a89beb3da94b00edbb4e769c047c7dfc9e
julien-lav <julien.laville@hotmail.fr>
2018-01-23 21:08:16 +0100

    heroku_fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,8 +78,8 @@ Rails.application.configure do
       address:              'smtp.gmail.com',
       port:                 587,
       domain:               'gmail.com',
-      user_name:            '',
-      password:             '',
+      user_name:            'info.prepa.cinema@gmail.com',
+      password:             'jujuantoine',
       authentication:       :plain,
       enable_starttls_auto: true
       }
